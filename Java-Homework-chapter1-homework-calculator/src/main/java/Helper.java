5f8a40b01f4f61edc8b746be0299fa8b0130a6e2
Dainius Karalevicius <dainius.karalevicus@gmail.com>
2020-11-02 09:52:12 +0200

    Finished old school BIG Class method Calculator, missing some validations Helper static methods, but it's enough for this evening :) for this I spend five hours... :)...

--- Java-Homework-chapter1-homework-calculator/src/main/java/Helper.java
+++ Java-Homework-chapter1-homework-calculator/src/main/java/Helper.java
@@ -1,7 +1,7 @@
 public class Helper {
 
     public static int factorial(int number) {
-        // comment for new commit.
+        // comment for new commit...
         int factorial = 1;
         for (int i = 1; i < number + 1; i++) {
             factorial *= i;
--- Java-Homework-chapter1-homework-calculator/src/main/java/Main.java
+++ Java-Homework-chapter1-homework-calculator/src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-
+// comment for new commit...
         Calculator calculator = new Calculator();
         System.out.println(calculator.calculate("5x1+2-4!"));
 
